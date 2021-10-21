9db74d929195a46029d95b730a14160a489922f3
billface <92800124+billface@users.noreply.github.com>
2021-10-21 06:41:42 +0100

    using FetchColumn to display one joke

--- public/index.php
+++ public/index.php
@@ -2,7 +2,13 @@
 try {
   $pdo = new PDO('mysql:host=localhost;dbname=room4Two', 'room4TwoUser', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $output = 'Database connection established.';
+  $sql = 'SELECT `blogtext` FROM `blog` WHERE id = :id';
+
+$statement = $pdo->prepare($sql);
+$statement->execute( ['id' => 1] );
+$text = $statement->fetchColumn();
+
+$output = $text;
 }
 catch (PDOException $e) {
   $output = 'Unable to connect to the database server: ' . $e;
