a7cc1d25be18273f91c0663ba3d3321016efdbe4
billface <92800124+billface@users.noreply.github.com>
2021-10-21 10:59:18 +0100

    Incudes Layout With Blog Of The Day

---
+++ public/blogs.php
@@ -0,0 +1,30 @@
+<?php
+
+try {
+  $pdo = new PDO('mysql:host=localhost;dbname=room4Two', 'room4TwoUser', 'mypassword');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = 'SELECT `blogtext` FROM `blog`';
+  $result = $pdo->query($sql);
+
+  while ($row = $result->fetch()) {
+    $blogs[] = $row['blogtext'];
+ }
+
+  $title = 'blog list';
+
+  ob_start();
+
+  include  __DIR__ . '/../templates/blogs.html.php';
+
+  $output = ob_get_clean();
+
+}
+catch (PDOException $e) {
+  $title = 'An error has occurred';
+
+  $output = 'Database error: ' . $e->getMessage() . ' in ' .
+  $e->getFile() . ':' . $e->getLine();
+}
+
+include  __DIR__ . '/../templates/layout.html.php';
